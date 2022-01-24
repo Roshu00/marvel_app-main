4bb26ff97d8fa8a02edf40a53585b7e90e6d2a46
Roshu00 <urosninkovic@yahoo.com>
2022-01-24 17:07:26 +0100

    New feature to Jumbotron

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -7,6 +7,12 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import CardContent from "@mui/material/CardContent";
 import Container from "@mui/material/Container";
+import SearchIcon from "@mui/icons-material/Search";
+import MouseIcon from "@mui/icons-material/Mouse";
+import AddIcon from "@mui/icons-material/Add";
+import ListItem from "@mui/material/ListItem";
+import List from "@mui/material/List";
+import ListSubheader from "@mui/material/ListSubheader";
 
 const JumbotronItem = ({ imageUrl, title, text }) => {
   return (
@@ -31,12 +37,48 @@ const Jumbotron = () => {
         minHeight: "100vh",
       }}
     >
-      <Typography
-        variant="h2"
-        sx={{ textAlign: "center", margin: "20px", color: "#101010 " }}
-      >
-        App created to help you find information about Marvel comics!
-      </Typography>
+      <Container>
+        <Typography
+          variant="h4"
+          sx={{ textAlign: "center", margin: "20px", color: "#101010 " }}
+        >
+          App created to help you find information about Marvel comics!
+        </Typography>
+        <List
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            marginY: "20px",
+          }}
+        >
+          <Typography variant="h4" color="#ED1D24">
+            How to?
+          </Typography>
+          <ListItem divider>
+            <SearchIcon fontSize="large" sx={{ margin: "10px" }} />
+            <Typography variant="p">
+              Enter starting words of the comic that you are serching for in
+              search feald in navigation bar and press enter key to start
+              searching
+            </Typography>
+          </ListItem>
+          <ListItem divider>
+            <MouseIcon fontSize="large" sx={{ margin: "10px" }} />
+            <Typography variant="p">
+              To see more information about comic click on it and window with
+              more information will bee shown
+            </Typography>
+          </ListItem>
+          <ListItem>
+            <AddIcon fontSize="large" sx={{ margin: "10px" }} />
+            <Typography variant="p">
+              Too see more comic that match your search click button "Load more"
+              on the bottom of the list and more comics will be loaded
+            </Typography>
+          </ListItem>
+        </List>
+      </Container>
       <JumbotronItem
         imageUrl={MarvelLogo}
         title={`This data is provided by Marvel API`}
